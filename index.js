a13e3a1cea054c9a07b2eca495f5ba43d75f8443
Edgar Hernandez <zdededg@gmail.com>
2023-07-07 14:32:20 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// findMatching function
+function findMatching(drivers, query) {
+  return drivers.filter(driver =>
+    driver.toLowerCase() === query.toLowerCase()
+  );
+}
+
+// fuzzyMatch function
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver =>
+    driver.toLowerCase().startsWith(query.toLowerCase())
+  );
+}
+
+// matchName function
+function matchName(driverObjects, query) {
+  return driverObjects.filter(driver =>
+    driver.name.toLowerCase() === query.toLowerCase()
+  );
+}
